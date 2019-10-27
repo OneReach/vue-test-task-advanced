c5e0dbc284fa72279f8e711efb4919b831823692
unknown <imohonyko@gmail.com>
2019-10-27 13:57:34 +0200

    autocomplete component added

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,56 @@
 <template>
   <div>
-    <div class="box">asdasd</div>
-    <div class="box">asdasd</div>
+    <div class="box">
+      <AutocompleteInput :value="inputValue" :getItems="getItems" v-model="inputValue" />
+    </div>
   </div>
 </template>
 
 <script>
+import AutocompleteInput from "./components/AutocompleteInput";
+
 export default {
-  name: 'Autocomplete app'
-}
+  name: "Autocomplete-app",
+  components: {
+    AutocompleteInput
+  },
+  data() {
+    return {
+      inputValue: "",
+      data: [
+        { id: 1, name: "Barney Gumble", img: "https://www.image-illustration.net/wp-content/uploads/2017/11/sunrise-background-wallpaper-vector.jpg" },
+        { id: 2, name: "Bart Simpson", img: "https://www.image-illustration.net/wp-content/uploads/2017/11/sunrise-background-wallpaper-vector.jpg" },
+        { id: 3, name: "Clancy Wiggam", img: "https://www.image-illustration.net/wp-content/uploads/2017/11/sunrise-background-wallpaper-vector.jpg" },
+        { id: 4, name: "Homer Simpson", img: "https://www.image-illustration.net/wp-content/uploads/2017/11/sunrise-background-wallpaper-vector.jpg" },
+        { id: 5, name: "Lisa Simpson", img: "https://www.image-illustration.net/wp-content/uploads/2017/11/sunrise-background-wallpaper-vector.jpg" },
+        { id: 6, name: "Maggie Simpson", img: "https://www.image-illustration.net/wp-content/uploads/2017/11/sunrise-background-wallpaper-vector.jpg" },
+        { id: 7, name: "Marge Simpson", img: "https://www.image-illustration.net/wp-content/uploads/2017/11/sunrise-background-wallpaper-vector.jpg" },
+        { id: 8, name: "Ned Flanders", img: "https://www.image-illustration.net/wp-content/uploads/2017/11/sunrise-background-wallpaper-vector.jpg" },
+        { id: 9, name: "Ralf Wiggam", img: "https://www.image-illustration.net/wp-content/uploads/2017/11/sunrise-background-wallpaper-vector.jpg" }
+      ]
+    };
+  },
+  methods: {
+    async getItems(text) {
+      return {
+        abort: () => {
+          console.log(`Abort function called with value ${text}`);
+        },
+        getResult: () => {
+          return new Promise((resolve, reject) => {
+              resolve(this.filterItems(text));
+          });
+        }
+      };
+    },
+    filterItems(text) {
+      return this.data.filter(item => item.name.toLowerCase().indexOf(text.toLowerCase()) !== -1);
+    }
+  }
+};
 </script>
 
 <style lang="scss">
-
 body {
   background: #fff;
   margin: 0;
@@ -30,5 +68,4 @@ body {
   margin: 50px auto;
   border-radius: 5px;
 }
-
 </style>
--- src/components/AutocompleteInput.vue
+++ src/components/AutocompleteInput.vue
@@ -1,14 +1,171 @@
 <template>
   <div>
+    <div class="inputBox">
+      <div class="inputBox__input">
+        <input
+          ref="inputElem"
+          @blur="hideSuggestions"
+          @focus="showSuggestions"
+          v-model="searchText"
+          type="text"
+        />
+      </div>
+      <div class="inputBox__controls">
+        <button @click="clearInputText" class="input-control-btn">X</button>
+      </div>
 
+      <div v-if="suggestionsVisibility" class="inputBox__suggestiongs">
+
+        <div :key="suggestion.id" v-for="suggestion in suggestions" class="search-result">
+          <div class="search-result__img">
+            <img :src="suggestion.img"/>
+          </div>
+          <div class="search-result__label">{{ suggestion.name }}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Autocomplete input'
-}
+  name: "Autocomplete-input",
+  props: {
+    value: {
+      type: String,
+      default: ""
+    },
+    getItems: {
+      type: Function,
+      required: true
+    }
+  },
+  data() {
+    return {
+      searchText: this.value,
+      suggestions: [],
+      suggestionsVisibility: false,
+      prevPromise: null
+    };
+  },
+  watch: {
+    searchText(val) {
+      this.$emit("input", val);
+
+      if (val.length === 0) {
+        this.hideSuggestions();
+      } else {
+        this.showSuggestions();
+      }
+
+      if(this.prevPromise !== null) {
+        this.prevPromise.abort();
+      }
+
+      this.getItems(val)
+        .then(result => {
+          this.prevPromise = result;
+          return result.getResult();
+        })
+        .then(result => {
+          this.suggestions = result;
+          this.prevPromise = null;
+          console.log(this.suggestions)
+        });
+    }
+  },
+  methods: {
+    hideSuggestions() {
+      this.suggestionsVisibility = false;
+    },
+    showSuggestions() {
+      if (this.searchText.length > 0) {
+        this.suggestionsVisibility = true;
+      }
+    },
+    clearInputText() {
+      this.searchText = "";
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
+.inputBox {
+  background: #fff;
+  border: 1px solid #c2e3f2;
+  border-radius: 3px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+  background: #f5f5f5;
+  cursor: text;
+
+  &__input {
+    width: 100%;
+    input {
+      padding: 15px;
+      border: none;
+      width: 100%;
+      outline: none;
+      background: transparent;
+    }
+  }
+
+  &__controls {
+    display: flex;
+  }
+
+  &__suggestiongs {
+    position: absolute;
+    top: calc(100% + 1px);
+    width: 100%;
+    border-radius: 3px;
+    background: #fff;
+    overflow: hidden;
+    z-index: 100;
+    box-shadow: 0 1px 4px rgba(0, 0, 0, 0.1);
+  }
+}
+
+.input-control-btn {
+  padding: 15px;
+  cursor: pointer;
+  outline: none;
+  border: none;
+  background: transparent;
+
+  &:hover {
+    background: #f2f2f2;
+  }
+}
+
+.search-result {
+  display: flex;
+  align-items: center;
+  padding: 10px;
+
+  &:hover {
+    background: #e6e6e6;
+    cursor: pointer;
+  }
+
+  &__img {
+    margin-right: 15px;
+    width: 35px;
+    height: 35px;
+    border-radius: 50%;
+    border: 1.5px solid red;
+    overflow: hidden;
+
+    img {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  &__label {
+  }
+}
 </style>
